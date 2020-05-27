c2aaec6fe2877c08992dcd26b6b9906607ba6ffd
0chen1D <214360639@qq.com>
2020-05-27 18:15:14 +0800

    添加一个键盘按键监听

--- src/com/zhangchi/tank/TankFrame.java
+++ src/com/zhangchi/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.zhangchi.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -24,10 +26,17 @@ public class TankFrame extends Frame{
 		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e){
-				System.out.print("123");
+				System.out.print("窗口关闭");
 				System.exit(0);
 			}
 		});
+		
+		//获得一个键盘监听 
+		this.addKeyListener(new  MyKeyListener());
+		
+		
+		
+		
 	}
 	
 	//Graphics是系统的一直画笔 paint方法会被自动调用
@@ -36,11 +45,28 @@ public class TankFrame extends Frame{
 		//画一个矩形  左上角为 0 0   长和高分别为50 
 		g.fillRect(x, y, 50, 50);
 		System.out.println("重画一次");
-		x=x+50;
-		y=y+50;
+		/*x=x+50;
+		y=y+50;*/
 		
 	}
 	
+	//定义一个内部类
+	class MyKeyListener extends KeyAdapter{
+		
+		//当一个按键被按下的时候触发
+		@Override
+		public void keyPressed(KeyEvent e){
+			System.out.println("健被按下了");
+			x=x+20;
+			repaint();
+		}
+		
+		//当一个按键被抬起来的时候触发
+		@Override
+		public void keyReleased(KeyEvent e){
+			System.out.println("健被抬起了");
+		}
+	}
 	
 
 }
